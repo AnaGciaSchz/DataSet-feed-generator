80bdebc8d697b5022da8c2750d4df45d35f86617
Ana María García Sánchez <anamariagarciasanchez@MacBook-Pro-de-Ana.local>
2021-08-31 12:44:55 +0200

    Logic to write the grocery feed done and working

--- logic/CreateGroceryFeed.js
+++ logic/CreateGroceryFeed.js
@@ -1,20 +1,29 @@
 var fr = require ("../inout/FolderReader")
 var fw = require ("../inout/FileWriter")
 
-exports.createFeed = async ()=> {
+exports.createFeed = function () {
     var directories = fr.getDirectories("resources/groceryStore")
-    console.log(directories);
-
-/** 
-var id = 62300701;
-var i;
-for (i = 0; i<10000;i++){
-    var data = await getData.getBasicInformation(id+i);
-    if(data.res != "error"){
-        console.log(data);
-        fw.write(data.name+"\t"+data.supplier+"\t"+data.category+"\t"+data.image+"\n");
+    for(directory in directories){//[ 'Fruit', 'Packages', 'Vegetables' ]
+        var folders = fr.getDirectories("resources/groceryStore/"+directories[directory]);
+        for(folder in folders){//[ 'Apple', 'Avocado', 'Banana', ... ]
+            var productNames = fr.getDirectories("resources/groceryStore/"+directories[directory]+"/"+folders[folder]);
+            
+            if(productNames.length >0){
+            for(productName in productNames){//[ 'Golden-Delicious', 'Granny-Smith', 'Pink-Lady', ... ]
+            writeInDocument(directories[directory],folders[folder],productNames[productName]);
+        }
+        }//if
+        else{
+            writeInDocument(directories[directory],folders[folder],folders[folder]);
+        }
     }
-}
-*/
+    }
 return "done";
+}
+
+function writeInDocument(directory,folder,productName){
+    var name = productName;
+    var category = "Grocery";
+    var image = "https://raw.githubusercontent.com/marcusklasson/GroceryStoreDataset/master/dataset/iconic-images-and-descriptions/"+directory+"/"+folder+"/"+name+"/"+name+".jpg";
+    fw.write(name+"\t"+category+"\t"+image+"\n");
 }
